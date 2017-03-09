9deaee704728cca6757569d087bdbede752894f0
Aleksandar Kandzhicki <akandzhichki@gmail.com>
2017-03-09 23:52:23 +0200

    Add client info and serialization methods

--- shOOP/Client.h
+++ shOOP/Client.h
@@ -6,14 +6,18 @@ class Client
 	std::string name;
 	// we want this ? Address address;
 
-	// company, person ...
+	// type - 0 = not known yet, 1 = company, 2 = person
 	int type;
 	std::string EIK;
 	std::string IN;
+	std::string serialized;
 public:
 	Client();
 	Client(std::string _name, int _type, std::string _EIK, std::string _IN);
 	void changeName(std::string new_name);
+	void info();
+	std::string serialize();
+	Client* deserialize(std::string);
 	~Client();
 };
 
