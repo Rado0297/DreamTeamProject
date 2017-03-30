47dadc04b2deca48d3988a449e720058df3d3dc7
Rado0297 <radoslavradov@gmail.com>
2017-03-30 21:28:51 +0300

    case class is here.

---
+++ shOOP/Case.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include "Case.hpp"
+#include "Client.h"
+
+Case::Case(double amountOfMoney, int caseID, bool isClosed, std::string workerID, std::string workerPassword, std::string bossID, std::string bossPassword)
+{
+
+}
+
+void Case::openCase()
+{
+	isClosed = false;
+}
+
+void Case::closeCase()
+{
+	isClosed = true;
+}
+
+void Case::addProductToBill()
+{
+	std::vector<Product> products;
+
+}
+
+void Case::emptyCase()
+{
+	std::string ID;
+	std::string password;
+	std::cout << "Enter worker ID: \n";
+	std::cin >> ID;
+	std::cout << "Enter worker password: \n";
+	std::cin >> password;
+
+	if (ID == bossID && password == bossPassword)
+		amountOfMoney = 0;
+	/*
+	else
+	{
+	std::cout << "3 attempts left:";
+	}
+	*/
+}
+
+double Case::getAmountOfMoney()
+{
+	return amountOfMoney;
+}
+
+double Case::raiseAmountOfMoney()
+{
+	//amountOfMoney += clientBill;
+	return amountOfMoney;
+}
+
+void Case::workerAuth()
+{
+	std::string ID;
+	std::string password;
+	std::cout << "Enter worker ID: \n";
+	std::cin >> ID;
+	std::cout << "Enter worker password: \n";
+	std::cin >> password;
+
+	/*
+	if(ID == workerID from DB && password == workerPassword with password, which matching to this ID)
+	worker is authenticated and ready for work
+	else
+	there is a problem and some one try to steal from case with brut-force attack
+
+	*/
+
+	openCase();
+}
+
+
+//destroctor
+Case::~Case()
+{
+
+}
---
+++ shOOP/Case.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+class Case
+{
+public:
+	//constructor
+	Case(double amountOfMoney, int caseID, bool isClosed, std::string workerID, std::string workerPassword, std::string bossID, std::string bossPassword);
+
+	//methods
+	void openCase();
+	void closeCase();
+
+	void addProductToBill();
+
+	//void emptyCase(std::string bossID, std::string bossPassword);
+	void emptyCase();
+
+	double getAmountOfMoney();
+
+	double raiseAmountOfMoney();
+
+	void workerAuth();
+
+	//destructor
+	~Case()
+	{
+
+	};
+private:
+
+	/*
+	dynamical array for products of each client
+	*/
+
+	double amountOfMoney;
+	int caseID;
+	bool isClosed = true;
+	std::string workerID;
+	std::string workerPassword;
+	std::string bossID;
+	std::string bossPassword;
+	std::string paymentMethod;
+};
